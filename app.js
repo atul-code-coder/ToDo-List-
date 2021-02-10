e2cc82ae9a132cb53169f21195d4e6260955e6b3
atul-code-coder <atulpatel22777@gmail.com>
2021-02-10 12:35:29 +0530

    .gitignore is now working

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express=require("express");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
@@ -10,7 +11,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-atul:Atul123@cluster0.6pxhz.mongodb.net/todolistDB",{useNewUrlParser:true});
+mongoose.connect("mongodb+srv://admin-atul:process.env.MONGODBID/todolistDB",{useNewUrlParser:true});
 
 const itemsSchema= new mongoose.Schema({
   name: String
